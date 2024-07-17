927492a99258364c2038c5010e2c5a379c867b10
Amrata MOHAMED <amrata.m.mohamed@gmail.com>
2024-07-17 17:33:12 +0200

    solved lab

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`;
 
 // Print out the concatenated string
 
-
+console.log(tongueTwister);
 
 
 /*******************************************
@@ -23,10 +24,48 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+/* let javDown = "jav";
+const = javDown.toLowerCase();
+let aUp = "a";
+const = aUp.toUpperCase();
+
+
+let scripDown = "scrip";
+const = scripDown.toLowerCase();
+let tUp = "t";
+const = tUp.toUpperCase();
+
+
+const result = `${javDown}${aUp}${scripDown}${tUp}`; */
+
+
+/* const part1Up = message.charAt(3); 
+const part2Up = message.charAt(5);
+
+const part1Down = part1.slice(0, 2);
+const part2Down = part2.slice(0, 4);
+
+const part1UpDown = `${part1Down}${part1Up}`;
+const part2UpDown = `${part2Down}${part2Up}`;
+
+const result = `${part1UpDown}${part2UpDown}`; */
+
+// OR
+
+/* let part1BeforeLast = part1.slice(0, part1.length-1);
+let lastLetter1Uppercase = word.slice(-1).toUpperCase();
+let part1WithUppercase = part1BeforeLast + lastLetter1Uppercase;
+
+let part2BeforeLast = part2.slice(0, part2.length-1);
+let lastLetter2Uppercase = word.slice(-1).toUpperCase();
+let part2WithUppercase = part2BeforeLast + lastLetter2Uppercase;
+
+const result = `${part1WithUppercase}${part2WithUppercase}`; */
+
 
 // Print the cameLtaiL-formatted string
 
-
+ // console.log(result);
 
 
 /*******************************************
@@ -36,10 +75,13 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
+const tip = 15/100;
+
+const tipAmount = billTotal*tip;
 
 // Print out the tipAmount
 
-
+console.log(tipAmount);
 
 
 /*******************************************
@@ -48,10 +90,13 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
+const randomCalc = Math.random() * 10;
+
+const randomNumber = Math.round(randomCalc);
 
 // Print the generated random number
 
-
+console.log(randomNumber);
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -62,15 +107,32 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+// False : because b is false.
 
 const expression2 = a || b;
+// True : because a is true.
 
 const expression3 = !a && b;
+// False : because both of them are false.
 
 const expression4 = !(a && b);
+// True : because the inside of the parenthesis is false, so the contrary is true. 
 
 const expression5 = !a || !b;
+// True : because, the second one is true. 
 
 const expression6 = !(a || b);
+// False : because, the inside of the parenthesis is true, so the contrary is false. 
 
 const expression7 = a && a;
+// True : because, a is true. 
+
+// Print out each expression.
+
+console.log(expression1);
+console.log(expression2);
+console.log(expression3);
+console.log(expression4);
+console.log(expression5);
+console.log(expression6);
+console.log(expression7);
